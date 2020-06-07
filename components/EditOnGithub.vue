31e2f8e1cd122d2dd2cf374fb7a08845d0c8694d
Richard Schloss <richard.e.schloss@gmail.com>
2020-06-07 01:05:45 -0700

    Updated edit on github

--- components/EditOnGithub.vue
+++ components/EditOnGithub.vue
@@ -17,7 +17,7 @@ export default {
   },
   computed: {
     link () {
-      return `https://github.com/richardeschloss/nuxt-socket-io-docs/edit/master/docs/content${this.document.path}${this.document.extension}`
+      return `https://github.com/richardeschloss/nuxt-socket-io-docs/edit/master/content/en/${this.document.path}${this.document.extension}`
     }
   }
 }
